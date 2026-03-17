7cbaf94b81bf3904f7448558d57dac84122fdfaf
Shaedow2000 <shaedow2000@proton.me>
2026-03-17 13:53:45 +0000

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/WebCalc',
-  root: '.',
-  build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: './index.html'
-        }
-  },
+  base: '/WebCalc/',
   plugins: [
     tailwindcss(),
   ],
